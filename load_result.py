49c8455aefb4e8afffe9da4ad4a3b3c2016e2583
kuriuminoki <mkuriu@outlook.jp>
2025-04-23 20:58:35 +0900

    mod

--- load_result.py
+++ load_result.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 """
@@ -40,7 +41,7 @@ def calc_total_accuracy(file_path, n1, n2):
     all_results_path = file_path.format("union{}and{}".format(n1, n2))
     print("write file : " + all_results_path)
     all_results = all_results.reset_index(drop=True)
-    all_results.to_csv(all_results_path)
+    all_results.to_csv(all_results_path, index=False)
 
 
 def combine_nonp_para_result(nonparametric_path, parametric_path, n, output_path):
@@ -72,22 +73,52 @@ def combine_nonp_para_result(nonparametric_path, parametric_path, n, output_path
     output_path = output_path.format(n)
     print("write file : " + output_path)
     nonpa_result = nonpa_result.reset_index(drop=True)
-    nonpa_result.to_csv(output_path)
+    nonpa_result.to_csv(output_path, index=False)
 
 
-def extract_only_correct(input_path, vanilla_correct, ret_correct ,output_path):
+def extract_only_correct(input_path, output_path):
     """
-    vanilla, retの結果でフィルタリングする。
-    vanilla_correct = Trueならvanillaで正解したデータを抽出
-    ret_correct = Trueならretで正解したデータを抽出
+    vanilla, retの結果が違うデータのみでフィルタリングする。
     input_path: 入力ファイル
     output_path: 抽出結果（フィルタリング結果）の書き込み先
     """
     result = pd.read_csv(input_path)
-    extracted_result = result.copy()
-    extracted_result = extracted_result[extracted_result.is_correct == vanilla_correct]
-    extracted_result = extracted_result[extracted_result.ret_is_correct == ret_correct]
-    extracted_result.to_csv(output_path)
+    extracted_result_list = list()
+
+    extracted_result1 = result.copy()
+    extracted_result1 = extracted_result1[extracted_result1.is_correct == True]
+    extracted_result1 = extracted_result1[extracted_result1.ret_is_correct == False]
+    extracted_result_list.append(extracted_result1)
+
+    extracted_result2 = result.copy()
+    extracted_result2 = extracted_result2[extracted_result2.is_correct == False]
+    extracted_result2 = extracted_result2[extracted_result2.ret_is_correct == True]
+    extracted_result_list.append(extracted_result2)
+
+    pd.concat(extracted_result_list).to_csv(output_path, index=False)
+
+
+def add_log_pop_column(input_path, summary_path, output_path):
+    """
+    s_popの対数を計算し、そのカラムを追加する。
+    また、s_popが閾値より値が大きいかを判定したカラムと閾値のカラムも追加する。
+    """
+    result = pd.read_csv(input_path)
+    log_pop = np.log(result["s_pop"].values)
+    result["log_pop"] = log_pop
+
+    result_list = list()
+    summary = pd.read_csv(summary_path)
+    summary = summary.set_index('prop')
+    props = result.prop.unique()
+    for prop in props:
+        prop_result = result[result.prop == prop].copy()
+        threshold = summary.at[prop, "thresnold"]
+        prop_result["use_ret"] = prop_result.log_pop < threshold
+        prop_result["thresnold"] = threshold
+        result_list.append(prop_result)
+    
+    pd.concat(result_list).to_csv(output_path, index=False)
 
 
 def main():
@@ -108,14 +139,20 @@ def main():
         output_path = "results/discussion/model=EleutherAI_gpt-neox-20b-input=None-method=vanillaVSBM25-shots=15-n={}_int8bit.csv"
         combine_nonp_para_result(nonp_result_path, para_result_path, 2000, output_path)
     
+    # 正解状況でフィルタリング
     if True:
         n = 2000
         input_path = f"results/discussion/model=EleutherAI_gpt-neox-20b-input=None-method=vanillaVSBM25-shots=15-n={n}_int8bit.csv"
-        vanilla_correct = False
-        ret_correct = True
-        output_path = f"results/discussion/model=EleutherAI_gpt-neox-20b-input=None-method=vanillaVSBM25-shots=15-n={n}_vanilla={vanilla_correct}-ret={ret_correct}int8bit.csv"
+        output_path = f"results/discussion/model=EleutherAI_gpt-neox-20b-input=None-method=vanillaVSBM25-shots=15-n={n}_filter_int8bit.csv"
         print("output_file: {}".format(output_path))
-        extract_only_correct(input_path, vanilla_correct, ret_correct, output_path)
+        extract_only_correct(input_path, output_path)
+    
+    if True:
+        n = 2000
+        input_path = f"results/discussion/model=EleutherAI_gpt-neox-20b-input=None-method=vanillaVSBM25-shots=15-n={n}_filter_int8bit.csv"
+        summary_path = f"results/discussion/model=EleutherAI_gpt-neox-20b-input=None-shots=15-n=union1400and2000_int8bit-ar_summary.csv"
+        output_path = f"results/discussion/model=EleutherAI_gpt-neox-20b-input=None-method=vanillaVSBM25-shots=15-n={n}_filter_spop_int8bit.csv"
+        add_log_pop_column(input_path, summary_path, output_path)
 
 
 if __name__ == "__main__":
